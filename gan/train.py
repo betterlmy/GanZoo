88c984bd55e93ec0945ef16f3166992cd00b114f
Zane <betterlmy@icloud.com>
2023-12-11 14:42:56 +0800

    重构模型代码

--- gan/train.py
+++ gan/train.py
@@ -3,10 +3,10 @@ from torchvision.utils import save_image
 from torchvision import transforms
 from torch.utils.data import Dataset
 from torch.autograd import Variable
-import torch.nn as nn
 import torch
 import os
 
+from gan.model import Generator, Discriminator
 from utils.CustomDataset import CDataset
 from utils import config
 from evalution import ssim, psnr, fid
@@ -21,60 +21,12 @@ cuda = True if torch.cuda.is_available() else False
 device = torch.device("cuda:" + configs['train'].gpu_id if cuda else "cpu")
 print("current device:" + str(device))
 
+adversarial_loss = torch.nn.BCELoss()  # 二分类交叉熵损失函数
 
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
-
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
-            if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.1, inplace=True))
-            return layers
-
-        self.model = nn.Sequential(
-            *block(model_config['latent_dim'], 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
-        )
-
-    def forward(self, z):
-        img = self.model(z)
-        img = img.view(img.size(0), *img_shape)
-        return img
-
-
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-
-        self.model = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(256, 1),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
-        validity = self.model(img_flat)
-        return validity
-
-
-# Loss function
-adversarial_loss = torch.nn.BCELoss()
-
-generator = Generator()
-discriminator = Discriminator()
+generator = Generator(model_config['latent_dim'],img_shape)  # 生成器
+discriminator = Discriminator(img_shape)  # 判别器
 
 if cuda:
-    device = torch.device("cuda:0")
     generator.to(device)
     discriminator.to(device)
     adversarial_loss.to(device)
@@ -112,35 +64,21 @@ def train():
     out_path = os.path.join(configs['project_dir'], "gan/output/")
     for epoch in range(train_config['n_epochs']):
         for i, imgs in enumerate(dataloader):
-            # Adversarial ground truths
             valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
             fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
 
-            # Configure input
             real_imgs = Variable(imgs.type(Tensor))
             optimizer_G.zero_grad()
-
-            # Sample noise as generator input
             z = Variable(
                 Tensor(np.random.normal(0, 1, (imgs.shape[0], model_config['latent_dim'])))
             )
-
-            # Generate a batch of images
             gen_imgs = generator(z)
 
-            # Loss measures generator's ability to fool the discriminator
             g_loss = adversarial_loss(discriminator(gen_imgs), valid)
-
             g_loss.backward()
             optimizer_G.step()
 
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
-
             optimizer_D.zero_grad()
-
-            # Measure discriminator's ability to classify real from generated samples
             real_loss = adversarial_loss(discriminator(real_imgs), valid)
             fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
             d_loss = (real_loss + fake_loss) / 2
@@ -166,27 +104,30 @@ def train():
                     nrow=5,
                     normalize=True,
                 )
+                # 保存模型
+                torch.save(generator.state_dict(), "gan_generator.pth")
+                torch.save(discriminator.state_dict(), "gan_discriminator.pth")
 
                 # 计算SSIM和PSNR
-                dataset2 = CDataset(image_dir, transform)
-                psnr_value = 0
-                ssim_value = 0
-                for i in range(10):
-                    max_psnr = 0
-                    max_ssim = 0
-                    for j in range(100):
-                        real_imgs = dataset2[i]
-                        ssim_value = ssim(gen_imgs.data[i], real_imgs.to(device))
-                        psnr_value = psnr(gen_imgs.data[i], real_imgs.to(device))
-                        if psnr_value > max_psnr:
-                            max_psnr = psnr_value
-                        if ssim_value > max_ssim:
-                            max_ssim = ssim_value
-
-                    psnr_value += max_psnr
-                    ssim_value += max_ssim
-
-                print("SSIM: %f, PSNR: %f" % (ssim_value / 10, psnr_value / 10))
+                # dataset2 = CDataset(image_dir, transform)
+                # psnr_value = 0
+                # ssim_value = 0
+                # for i in range(10):
+                #     max_psnr = 0
+                #     max_ssim = 0
+                #     for j in range(100):
+                #         real_imgs = dataset2[i]
+                #         ssim_value = ssim(gen_imgs.data[i], real_imgs.to(device))
+                #         psnr_value = psnr(gen_imgs.data[i], real_imgs.to(device))
+                #         if psnr_value > max_psnr:
+                #             max_psnr = psnr_value
+                #         if ssim_value > max_ssim:
+                #             max_ssim = ssim_value
+                #
+                #     psnr_value += max_psnr
+                #     ssim_value += max_ssim
+                #
+                # print("SSIM: %f, PSNR: %f" % (ssim_value / 10, psnr_value / 10))
 
 
 if __name__ == "__main__":
