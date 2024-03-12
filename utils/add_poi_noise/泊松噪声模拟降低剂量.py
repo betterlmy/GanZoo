2f8ef1906edfc9467e31737454d0cdde5dadbf57
Zane <betterlmy@icloud.com>
2024-03-12 20:12:44 +0800

    修改

--- utils/add_poi_noise/泊松噪声模拟降低剂量.py
+++ utils/add_poi_noise/泊松噪声模拟降低剂量.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 import os
 
@@ -23,17 +22,12 @@ def add_poisson_noise(image, scale=1.0):
 if __name__ == "__main__":
 
     # 创建文件夹
-    path = r"/root/lmy/aapm512/high/"
-    paths = [
-        os.path.join(path, "16331.png"),
-        os.path.join(path, "6.png"),
-        os.path.join(path, "500.png"),
-        os.path.join(path, "8000.png"),
-        os.path.join(path, "4000.png"),
-    ]
+    path_high = r"/root/lmy/aapm512/high/"
+    pics = ["15800", "6", "500", "8000", "4000"]
+
     os.makedirs("/root/lmy/GanZoo/utils/add_poi_noise/pics", exist_ok=True)
-    for i, path in enumerate(paths):
-        original_image = np.array(Image.open(path))
+    for i, pic_num in enumerate(pics):
+        original_image = np.array(Image.open(os.path.join(path_high, f"{pic_num}.png")))
         scales = [0.93, 0.62, 0.86, 0.52, 0.42, 0.71]
         for scale in scales:
             noisy_image = add_poisson_noise(original_image, scale=scale)
@@ -46,3 +40,7 @@ if __name__ == "__main__":
             Image.fromarray(original_image).save(
                 os.path.join("/root/lmy/GanZoo/utils/add_poi_noise/pics", f"{i}.png")
             )
+            Image.open(os.path.join(r"/root/lmy/aapm512/low/", f"{pic_num}.png")).save(
+                os.path.join("/root/lmy/GanZoo/utils/add_poi_noise/pics", f"{i}_low.png")
+            )
+
