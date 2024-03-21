aa65a626a2de3ec86e3fa46a44fa3bb6923f6153
Zane <betterlmy@icloud.com>
2024-03-21 22:58:51 +0800

    remove redcnn in dugan

--- dugan_p/train_dugan.sh
+++ dugan_p/train_dugan.sh
@@ -6,4 +6,4 @@ python main.py --batch_size=64 \
     --grad_loss_weight=35.310016043755894 --img_gen_loss_weight=0.14178356036938378 --max_iter=100000 \
     --model_name=DUGAN --num_channels=32 --num_layers=10 --num_workers=32 --pix_loss_weight=5.034293425614828 \
     --run_name=official --save_freq=2500 --test_batch_size=1 --test_dataset_name=cmayo_test_512 \
-    --train_dataset_name=cmayo_train_64 --use_grad_discriminator=true --weight_decay 0. --num_workers 4
+    --train_dataset_name=cmayo_train_64 --use_grad_discriminator=true --weight_decay 0. --num_workers 0
